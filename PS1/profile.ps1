154888235dffd05eeb411c30a2c6e40ad69d3281
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2023-02-19 19:44:20 -0800

    Add notepad++ alias and update FFMpeg location.

--- PS1/profile.ps1
+++ PS1/profile.ps1
@@ -1,17 +1,20 @@
 #Copyright 2023 Gilgamech Technologies
 #Author: Stephen Gillie
 #Created 2/4/2023
-#Updated 2/4/2023
+#Updated 2/19/2023
 #Notes: Import-Module C:\repos\Gilgamech\PS1\profile.ps1 -force
+
+New-Alias note "C:\Program Files\n++\notepad++.exe"
 
 function Get-YoutubeDL ($videoURI) {
 	& 'C:\Program Files\util\youtube-dl.exe' -x $videoURI
 }
 
+$ffmpegLoc = "C:\Users\Gilgamech\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-5.1.2-full_build\bin\ffmpeg.exe"
 function Convert-FileToMp3 ($FileName) {
 	$outName = Get-ChildItem $FileName
 	$outFile = ($outName.FullName[0..$outName.FullName.IndexOf(".")] -join "") + "mp3"
-	& 'C:\Program Files\ffmpeg-2022-12-15-git-9adf02247c-full_build\bin\ffmpeg.exe' -i $FileName -q:a 0 -map a $outFile
+	& $ffmpegLoc  -i $FileName -q:a 0 -map a $outFile
 }
 
 function Sort-Clipboard ($toSort = (Get-Clipboard)) {

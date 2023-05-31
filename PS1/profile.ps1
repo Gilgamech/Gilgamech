a7c999d7a27ab8bf01ce8d11263aa2d614362f6d
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2023-05-31 12:25:54 -0800

    Rearrange profile and add Parse-Text.

--- PS1/profile.ps1
+++ PS1/profile.ps1
@@ -5,7 +5,36 @@
 #Notes: Import-Module C:\repos\Gilgamech\PS1\profile.ps1 -force
 #And the love kickstarts again.
 
-#They say "break a leg" because "good luck" is bad luck and they want you to be "in the cast". 
+New-Alias note "C:\Program Files\n++\notepad++.exe"
+
+function Get-YoutubeDL ($videoURI) {
+	& 'C:\Program Files\util\youtube-dl.exe' -x $videoURI
+}
+
+$ffmpegLoc = "C:\Users\Gilgamech\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-5.1.2-full_build\bin\ffmpeg.exe"
+function Convert-FileToMp3 ($FileName) {
+	$outName = Get-ChildItem $FileName
+	$outFile = ($outName.FullName[0..$outName.FullName.IndexOf(".")] -join "") + "mp3"
+	& $ffmpegLoc  -i $FileName -q:a 0 -map a $outFile
+}
+
+function Sort-Clipboard ($toSort = (Get-Clipboard)) {
+	$toSort | sort | clip
+}
+
+function Get-File2Dir ($dir){
+	rm "$dir\file2dir.txt";
+	ls $dir | %{
+		$_.fullname |out-file "$dir\file2dir.txt" -Append;
+		gc $_.fullname |out-file "$dir\file2dir.txt" -Append
+	}
+}
+
+function Restart-ProgWebserver($i) {
+	copy .\inMemCacheFile.json .\imcs\$i.json
+	copy .\inMemCacheFileCopy.json .\inMemCacheFile.json;
+	node .\index.js
+}
 
 function Get-ScreenResolution {            
 [void] [Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")            
@@ -65,35 +94,7 @@ sleep 3
 }
 }
 
-
-New-Alias note "C:\Program Files\n++\notepad++.exe"
-
-function Get-YoutubeDL ($videoURI) {
-	& 'C:\Program Files\util\youtube-dl.exe' -x $videoURI
-}
-
-$ffmpegLoc = "C:\Users\Gilgamech\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-5.1.2-full_build\bin\ffmpeg.exe"
-function Convert-FileToMp3 ($FileName) {
-	$outName = Get-ChildItem $FileName
-	$outFile = ($outName.FullName[0..$outName.FullName.IndexOf(".")] -join "") + "mp3"
-	& $ffmpegLoc  -i $FileName -q:a 0 -map a $outFile
-}
-
-function Sort-Clipboard ($toSort = (Get-Clipboard)) {
-	$toSort | sort | clip
-}
-
-function Get-File2Dir ($dir){
-	rm "$dir\file2dir.txt";
-	ls $dir | %{
-		$_.fullname |out-file "$dir\file2dir.txt" -Append;
-		gc $_.fullname |out-file "$dir\file2dir.txt" -Append
-	}
-}
-
-function Restart-ProgWebserver($i) {
-	copy .\inMemCacheFile.json .\imcs\$i.json
-	copy .\inMemCacheFileCopy.json .\inMemCacheFile.json;
-	node .\index.js
+Function Parse-Text($openDelim,$closeDelim,$text){
+    ($text -replace $openDelim,"") -split $closeDelim
 }
 
